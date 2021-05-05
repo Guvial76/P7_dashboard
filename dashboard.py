6ab60e1d73536ce8f0db503d933cf622dec98dd3
Guvial76 <g_vial@me.com>
2021-05-05 10:13:08 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,27 +10,36 @@ This app provides information regarding credit decision based on customer scorin
 
 """)
 
-st.sidebar.header('Credit Application Number')
+# Load processed test data
+data = pd.read_csv('test_data.csv')
+df = data.drop('TARGET', axis = 1)
+df=df.drop('Unnamed: 0', axis=1)
 
+
+# Input and save Credit Application Number (SK_ID_CURR)
+st.sidebar.header('Credit Application Number')
 st.sidebar.markdown("""
 Please fill the SK_ID_CURR
 """)
+cust_id = int(st.sidebar.text_input('SK_ID_CURR', 100001))
 
-cust_id = st.sidebar.number_input('SK_ID_CURR',
-                                   min_value=100001., 
-                                   value=100013., 
-                                   step=1.)
 
-data = pd.read_csv('test_data.csv')
-
-df = data.drop('TARGET', axis = 1)
-
-# Read in saved classification model
+# Apply saved model to make predictions
 load_clf = joblib.load("trained_model.joblib")
+prediction_proba = load_clf.predict_proba(df.loc[df['SK_ID_CURR'] == cust_id])
+score = prediction_proba[0][1]*100
+thd = joblib.load("trained_model_threshold.joblib")*100
 
-# Apply model to make predictions
-prediction_proba = load_clf.predict_proba(df.loc[df['SK_ID_CURR'] = cust_id])
 
 # Display prediction
-st.subheader('Credit Score')
-st.write(prediction_proba[1])
+st.subheader('Credit Score (/100)')
+st.write(int(score))
+
+# Display decision
+st.subheader('Credit Decision')
+
+if score > thd :
+	st.write('Approved')
+
+else :
+	st.write('Refused')
